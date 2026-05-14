ce3d3b3bac1f7f97e1400946b16d6158a55cdfd0
Ibzan <ibzan@ibzan.co.uk>
2026-05-14 12:56:40 +0100

    Update next version

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMdx = nextMdx({})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withMdx({
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
